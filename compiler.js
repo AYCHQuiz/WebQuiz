f03563b9513d18e21ec513cde5fb313f1d9f9cd6
Hans Kirchner <hans-kirchner@gmx.de>
2016-11-11 22:34:52 +0100

    Add YAML question to HTML+SVG renderer
    TODO: permute answers for multiple choice

---
+++ compiler.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+main();
+
+function main() {
+    const args = process.argv.slice(2);
+    if(args.length !== 1) {
+        console.log("Usage: compiler.js <file>");
+        process.exit(1);
+    }
+
+    const renderFile = require("./lib/render.js").renderFile; // lazy-load
+
+    renderFile(args[0]).then(html => {
+        console.log(html);
+    }).catch(error =>  {
+        console.error(error);
+        process.exit(1);
+    });
+}
