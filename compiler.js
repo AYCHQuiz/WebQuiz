2733b54108d5c5c78be292423cd05b1cf1126e71
Hans Kirchner <hans-kirchner@gmx.de>
2016-11-12 20:20:01 +0100

    Improve compiler to process whole directories

--- compiler.js
+++ compiler.js
@@ -1,19 +1,69 @@
 #!/usr/bin/env node
+const ArgumentParser = require("argparse").ArgumentParser;
+const package = require("./package");
+const Bluebird = require("Bluebird");
+const fs = Bluebird.promisifyAll(require("fs"));
+const path = require("path");
+const renderFile = require("./lib/render.js").renderFile;
+
 main();
 
 function main() {
-    const args = process.argv.slice(2);
-    if(args.length !== 1) {
-        console.log("Usage: compiler.js <file>");
-        process.exit(1);
-    }
-
-    const renderFile = require("./lib/render.js").renderFile; // lazy-load
-
-    renderFile(args[0]).then(html => {
-        console.log(JSON.stringify(html));
-    }).catch(error =>  {
-        console.error(error);
-        process.exit(1);
+    const parser = new ArgumentParser({
+        version: package.version,
+        addHelp: true,
+        description: package.description
     });
+    parser.addArgument(["in"]);
+    parser.addArgument(["-o", "--out"], {
+        required: true
+    });
+    const args = parser.parseArgs();
+
+    if(fs.statSync(args.in).isDirectory()) {
+        try{ fs.mkdirSync(args.out); } catch(e) {}
+        compileDirectory(args.in, args.out);
+    } else {
+        compileFile(args.in, args.out);
+    }
+}
+
+function compileDirectory(src, dest) {
+    const allTags = new Set();
+    const questionIndex = [];
+
+    fs.readdirAsync(src).filter(fileName =>
+        [".yml", ".yaml"].includes(path.extname(fileName))
+    ).filter(fileName =>
+        fs.statAsync(path.join(src, fileName)).then(stat => stat.isFile())
+    ).map(fileName =>
+        renderFile(path.join(src, fileName))
+    ).each(question => {
+        const destFile = path.join(dest, question.uid + ".json");
+        fs.writeFileAsync(destFile, JSON.stringify(question.content), "utf8");
+        for(const tag of question.tags) {
+            allTags.add(tag);
+        }
+        questionIndex.push({
+            uid: question.uid,
+            tags: question.tags
+        });
+    }).then(() => {
+        fs.writeFileAsync(path.join(dest, "_index.json"), JSON.stringify({
+            questions: questionIndex,
+            tags: Array.from(allTags)
+        }), "utf8");
+    });
+}
+
+function compileFile(src, dest) {
+    renderFile(src).then(html => {
+        return fs.writeFileAsync(dest, JSON.stringify(html), "utf8");
+    }).catch(error => {
+        console.error(error);
+    });
+}
+
+function replaceFileExt(fileName, nextExt) {
+    return fileName.replace(/\.[^/\.]+$/, nextExt);
 }
