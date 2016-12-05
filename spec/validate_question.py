b66ac5ced21c4d0dc5c877ed26c67e5afedda521
Mikhail Pak <mikhail.pak@tum.de>
2016-12-06 00:18:09 +0100

    Added UID uniqueness checker to the Python validator

--- spec/validate_question.py
+++ spec/validate_question.py
@@ -3,10 +3,12 @@
 
 from __future__ import print_function
 import argparse
+import codecs
 import fnmatch
 import os
 import sys
 import yamale
+import yaml
 
 
 def find_question_files(root_directory):
@@ -16,7 +18,13 @@ def find_question_files(root_directory):
             yield os.path.join(root, basename)
 
 
-def validate(schema_filename, data_filename):
+def get_uid(filename):
+    with codecs.open(filename, 'r', encoding="utf-8") as f:
+        doc = yaml.load(f)
+    return doc["uid"]
+
+
+def validate(schema_filename, data_filename, uids):
     """Validate a YAML file according to the supplied schema."""
     schema = yamale.make_schema(schema_filename)
     data = yamale.make_data(data_filename)
@@ -25,6 +33,12 @@ def validate(schema_filename, data_filename):
         print("")
         print("Checking file '{}'...".format(data_filename))
         yamale.validate(schema, data)
+        curr_uid = get_uid(data_filename)
+        if curr_uid in uids:
+            print("Invalid data: Non-unique UID '{:s}'".format(curr_uid))
+            return 2
+        else:
+            uids.add(curr_uid)
         print("Everything ok.")
         return 0
     except ValueError as err:
@@ -46,8 +60,9 @@ if __name__ == "__main__":
     if os.path.isfile(args.path):
         sys.exit(validate(args.schema, args.path))
     elif os.path.isdir(args.path):
+        uids = set()
         # Use eager evaluation here, otherwise program exits on first invalid file
-        exit_codes = [validate(args.schema, d) for d in find_question_files(args.path)]
+        exit_codes = [validate(args.schema, d, uids) for d in find_question_files(args.path)]
         if all(ec == 0 for ec in exit_codes):
             sys.exit(0)
         else:
