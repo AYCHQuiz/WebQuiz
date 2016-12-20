44044236238af4e39055fa0e956d80f79c0d2bf0
Hans Kirchner <hans-kirchner@gmx.de>
2016-12-20 02:07:56 +0100

    Migrate webpack config to webpack v2

--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,35 @@ module.exports = {
         filename: "static/js/bundle.js"
     },
     resolve: {
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".js"],
-        alias: {
-            //'vue$': 'vue/dist/vue.js'
-        }
+        extensions: [".webpack.js", ".web.js", ".ts", ".js"]
     },
     module: {
-        loaders: [
-            { test: /\.ts$/, loader: "vue-ts" },
-            { test: /\.vue$/, loader: "vue" },
-            { test: /\.css$/, loader: "style-loader!css-loader" },
-            { test: /\.(eot|svg|ttf|woff|woff2)(\?v=[0-9]+\.[0-9]+\.[0-9]+)?$/,
-              loader: "file?name=static/fonts/[name].[ext]"}
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "vue-ts-loader"
+            },
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+                options: {
+                    // instruct vue-loader to load TypeScript
+                    loaders: { js: "vue-ts-loader" },
+                    // make TS' generated code cooperate with vue-loader
+                    esModule: true
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: "file-loader",
+                query: {
+                    name: "static/fonts/[name].[ext]"
+                }
+            }
         ]
-    },
-    vue: {
-        // instruct vue-loader to load TypeScript
-        loaders: { js: "vue-ts-loader" },
-        // make TS' generated code cooperate with vue-loader
-        esModule: true
     }
 };
