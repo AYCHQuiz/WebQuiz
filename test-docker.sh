edb1c73b20bfd5185758e42d2756bcc3d412a724
Hans Kirchner <hans-kirchner@gmx.de>
2017-06-07 17:03:43 +0200

    Update test scripts for nvm and docker

--- test-docker.sh
+++ test-docker.sh
@@ -5,7 +5,5 @@
 # 2) ./test-docker.sh
 # 3) profit
 
-# There is no node:5 or node:5-alpine image
-docker run --rm -t -v$PWD:/usr/src/app -w/usr/src/app node:4-alpine npm test && \
 docker run --rm -t -v$PWD:/usr/src/app -w/usr/src/app node:6-alpine npm test && \
-docker run --rm -t -v$PWD:/usr/src/app -w/usr/src/app node:7-alpine npm test
+docker run --rm -t -v$PWD:/usr/src/app -w/usr/src/app node:8-alpine npm test
--- test-nvm.sh
+++ test-nvm.sh
@@ -3,16 +3,12 @@
 # Run test cases on all supported Node.js versions
 # 1) Install nvm: https://github.com/creationix/nvm
 # 2) Install all node versions:
-# 2.1) nvm install 4
-# 2.2) nvm install 5
-# 2.3) nvm install 6
-# 2.4) nvm install 7
+# 2.1) nvm install 6
+# 2.2) nvm install 8
 # 3) ./test-nvm.sh
 # 4) profit
 
 . "$HOME/.nvm/nvm.sh"  # This loads nvm
 
-nvm exec 4 npm test && \
-nvm exec 5 npm test && \
 nvm exec 6 npm test && \
-nvm exec 7 npm test
+nvm exec 8 npm test
